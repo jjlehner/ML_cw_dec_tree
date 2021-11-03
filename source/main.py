49f6a29a74e29b7e584a67e164a71f991a9abb91
Tanguy P <58601202+tlp19@users.noreply.github.com>
2021-11-03 20:53:57 +0000

    Follow more closely README specifications

--- source/main.py
+++ source/main.py
@@ -1,22 +1,18 @@
 from decision_tree import *
 
 if __name__ == '__main__':
-    # load both the clean and noisy datasets
+    # Load both the clean and noisy datasets
     clean_data = data.load_clean()
     noisy_data = data.load_noisy()
     
-    #train and test the tree over both datasets
+    # Train and evaluate the tree over both datasets
     print("\n\nStep 3 - Results with clean data:\n")
-    decision_tree_learning(clean_data, verbose=True)
+    tree_clean = decision_tree_learning(clean_data, verbose=True)
     print("\n\nStep 3 - Results with noisy data:\n")
-    decision_tree_learning(noisy_data, verbose=True)
+    tree_noisy = decision_tree_learning(noisy_data, verbose=True)
 
+    # Train, prune, and evaluate the tree over both datasets 
     print("\nStep 4 - Pruning results with clean data:")
-    decision_tree_learning(clean_data, pruning=True, verbose=True)
+    pruned_tree_clean = decision_tree_learning(clean_data, pruning=True, verbose=True)
     print("\n\nStep 4 - Pruning results with noisy data:")
-    decision_tree_learning(noisy_data, pruning=True, verbose=True)
-    
-    
-
-
-
+    pruned_tree_noisy = decision_tree_learning(noisy_data, pruning=True, verbose=True)
